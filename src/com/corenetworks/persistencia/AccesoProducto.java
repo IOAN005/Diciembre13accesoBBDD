0bea52c90776fe993cca15d78dd9d93da79c1d18
IOAN005 <nellbovinca@gmail.com>
2023-12-13 19:06:28 +0100

    class1

--- src/com/corenetworks/persistencia/AccesoProducto.java
+++ src/com/corenetworks/persistencia/AccesoProducto.java
@@ -1,2 +1,58 @@
-package com.corenetworks.persistencia;public class AccesoProducto {
+package com.corenetworks.persistencia;
+
+import com.corenetworks.modelo.Producto;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccesoProducto  extends Conexion {
+
+    public Producto obtenerUno(int id) throws SQLException, ClassNotFoundException {
+
+        Statement sentencia;
+        ResultSet resultado;
+        String sql = "Select product_id,product_name,unit_price,units_in_stock from public.products where product_id= " + id;
+        Producto p1 = null;
+        abrirConexion();
+
+        sentencia = miConexion.createStatement();
+        resultado = sentencia.executeQuery(sql);
+
+        if (resultado.next()) {
+
+            p1 = new Producto(resultado.getInt("product_id"),
+                    resultado.getString("product_name"),
+                    resultado.getDouble("unit_price"),
+                    resultado.getInt("units_in_stock"));
+
+        }
+        return p1;
+
+    }
+
+    public List<Producto> obtenerTodos() throws SQLException, ClassNotFoundException {
+
+        Statement sentencia;
+        ResultSet resultado;
+        String sql = "Select product_id,product_name,unit_price,units_in_stock from public.products  ";
+        List<Producto> productos=new ArrayList<>();
+        abrirConexion();
+
+        sentencia = miConexion.createStatement();
+        resultado = sentencia.executeQuery(sql);
+
+        while (resultado.next()) {
+            productos.add(new Producto(resultado.getInt("product_id"),
+                    resultado.getString("product_name"),
+                    resultado.getDouble("unit_price"),
+                    resultado.getInt("units_in_stock")));
+
+
+
+        }
+        return productos;
+    }
 }
--- src/com/corenetworks/presentacion/Main.java
+++ src/com/corenetworks/presentacion/Main.java
@@ -1,5 +1,40 @@
+package com.corenetworks.presentacion;
+
+import com.corenetworks.persistencia.AccesoProducto;
+import com.corenetworks.persistencia.Conexion;
+
+import java.sql.*;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.Executor;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+
+        Conexion c1 = new Conexion();
+        try {
+            c1.abrirConexion();
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.toString());
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+      AccesoProducto ap1= new AccesoProducto();
+//        try {
+//            System.out.println(ap1.obtenerUno(5));
+//        } catch (ClassNotFoundException e) {
+//            System.out.println(e.toString());
+//        } catch (SQLException e) {
+//            System.out.println(e.toString());
+//        }
+        try {
+            System.out.println(ap1.obtenerTodos());
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.toString());
+        }
     }
-}
+
+
+    }
